97bd643dfc180a51e849472c7b975a79b59fe264
Malcolm Bonar <malbonar@gmail.com>
2018-12-14 23:36:57 +1300

    next attempt at fixing routing

--- precache-manifest.c89b44944ca402c7ff632aefba2de0cd.js
+++ precache-manifest.c89b44944ca402c7ff632aefba2de0cd.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/trainerjockeyform/static/js/runtime~main.3657f0d9.js"
   },
   {
-    "revision": "427f5b2a692d0eaa402b",
-    "url": "/trainerjockeyform/static/js/main.427f5b2a.chunk.js"
+    "revision": "18ae78b0427abbd10c1c",
+    "url": "/trainerjockeyform/static/js/main.18ae78b0.chunk.js"
   },
   {
     "revision": "738551eb02bcf6481701",
     "url": "/trainerjockeyform/static/js/1.738551eb.chunk.js"
   },
   {
-    "revision": "427f5b2a692d0eaa402b",
+    "revision": "18ae78b0427abbd10c1c",
     "url": "/trainerjockeyform/static/css/main.bff4bc7f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/trainerjockeyform/static/css/1.1e4ea5f2.chunk.css"
   },
   {
-    "revision": "64e4949400d58d7b5cd081b531dfcc1e",
+    "revision": "1370d1009e4906aa249e241dacb66898",
     "url": "/trainerjockeyform/index.html"
   }
 ];
